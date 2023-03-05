ac396f524dd03ed0d0137f0ce5e35c94f6fe48a5
mega-kai <1968974979lv@gmail.com>
2023-03-05 17:29:27 +0800

    a

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+var favicon = require("serve-favicon");
+
+app.use(favicon(__dirname + "/favicon.ico"));
+
 // same with use but only deals with incoming get request of a certain path
 app.get("/", (req, res) => {
   res.send(req.ip);
