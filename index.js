3adc7af7b60855026453e29eaebaf5e2d64fa98e
mega-kai <1968974979lv@gmail.com>
2023-03-06 13:23:59 +0800

    a

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const favicon = require("serve-favicon");
-const path = require("path");
 
 const app = express();
-
-app.use(favicon(path.join(__dirname, "favicon.ico")));
 
 app.get("/", (req, res) => {
   res.send("hi there uwu");
